b62cf7c04f46c8be849cbe6585ef7abd5b3b543c
Santiago Rincon <santiagorincon2001@gmail.com>
2020-09-17 19:37:57 -0500

    Se agrega el setup para empaquetar el proyecto en whl

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="Api cupos condor",
+    version="1.0",
+    author="Santiago Rincon",
+    author_email="sanrinconr@correo.udistrital.edu.co",
+    description="Api para la busqueda de cupos en condor",
+    long_description="long_description",
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/cupos-condor/api",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
