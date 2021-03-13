0c8215aeadb2e6af3af7787144434c42ba410019
Santiago Rincon <santiagorincon2001@gmail.com>
2021-03-12 23:19:15 -0500

    Waitress y gunicorn

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,13 @@
 import os
 from app import create_app
+from waitress import serve
 
 settings_module = os.getenv("APP_SETTINGS_MODULE")
 app = create_app(settings_module)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    if os.environ.get("ENVIRONMENT","development") == "production":
+        app.debug = False
+        port = int(os.environ.get('PORT', 5000))
+        serve(app, port=port)
+    else:
+        app.run(host="0.0.0.0")
